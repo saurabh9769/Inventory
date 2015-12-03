3100028a91a5c4c2228bc61e3bb96235f87256ad
Saurabh Sangani <sanganisaurabh92@gmail.com>
2015-12-03 14:47:43 +0530

    inventory.rb

--- inventory.rb
+++ inventory.rb
@@ -1,61 +1,87 @@
-#class Inventory	
+require_relative './item.rb'
+
+class Inventory	
 	#p_name = 0, p_no = 0
-	$items =
-	{
-		'apple' => 10,
-		'mango' => 20,
-		'grape' => 30
-	}
 	
-	
-def buy()
-	puts "Enter a product name : \n apple mango grape"
-	p_name = gets.chomp
-	if $items[p_name]
-		puts "Enter  the quantity to be added or removed : "
-		p_no = gets.chomp.to_i
+	# $items =
+	# {
+	# 	'apple' => 10,
+	# 	'mango' => 20,
+	# 	'grape' => 30
+	# }
+
+	def initialize()	
+		@@items = []
+		a = Item.new('apple',30)
+		b = Item.new('orange',40)
+		c = Item.new('grape',50)
+		@@items.push(a)
+		@@items.push(b)
+		@@items.push(c)
+	end
+
+	def buy()
+		puts "Enter a product name : \n apple orange grape"
+		p_name = gets.chomp
+		item = @@items.select{|i| i.name == p_name}.last
+		if item
+			puts "Enter  the quantity to be added or removed : "
+			p_no = gets.chomp.to_i
 		
-		$items[p_name] = $items[p_name] + p_no
-		msg(p_name,$items[p_name])
-	else	
-		puts"invalid input"
+			item.quantity = item.quantity + p_no
+			#puts item.quantity
+			msg
+		else	
+			puts"invalid input"
+		end
 	end
-end
 
-def sell()
-	puts "Enter a product name : \n apple mango grape"
-	p_name = gets.chomp
-	if $items[p_name]
-		puts "Enter  the quantity to be added or removed : "
-		p_no = gets.chomp.to_i
-
-		$items[p_name] = $items[p_name] - p_no
-		msg(p_name,$items[p_name])
-	else	
-		puts"invalid input"
+	def sell()
+		puts "Enter a product name : \n apple orange grape"
+		p_name = gets.chomp
+		item = @@items.select{|i| i.name == p_name}.last
+		if item
+			puts "Enter  the quantity to be added or removed : "
+			p_no = gets.chomp.to_i
+			if p_no > item.quantity
+				puts "Insert proper quantity "
+			else
+				item.quantity = item.quantity - p_no
+				#puts item.quantity
+				msg
+			end
+		else	
+			puts"invalid input"
+		end
 	end
-end
 
-def msg(key,value)
-	puts"Your current inventory status is :  #{key}:#{value}"
-end
+	def msg()
+		puts"Your current inventory status is :  "
+		@@items.each do |i| 
+			puts i.name
+			puts i.quantity
+		end 
+	end	
+	def start()
 	option = 0
 	while option != 'exit' 
 		puts "Select an operation to perform :\n buy,sell,exit " 
 		option = gets.chomp
-	
+			
 		case option
 			when "buy" then 
 				buy 
-			#puts "item bought"
-			
+				#puts "item bought"
+					
 			when "sell" then 
 				sell  
-			#puts"item sold"
-			
+				#puts"item sold"
 			when "exit"  then break
 			else
 				puts " Invalid input "
+			end
 		end
 	end
+end
 
+Inventory.new.start
